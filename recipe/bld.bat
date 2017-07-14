3ed1b2aa6cadc303f8dafb5c085d5f2a06871159
Filipe Fernandes <ocefpaf@gmail.com>
2017-07-14 12:58:37 -0300

    Windows builds

---
+++ recipe/bld.bat
@@ -0,0 +1,13 @@
+copy "%RECIPE_DIR%\build.sh" .
+set PREFIX=%PREFIX:\=/%
+set SRC_DIR=%SRC_DIR:\=/%
+set MSYSTEM=MINGW%ARCH%
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
+bash -lc "./build.sh"
+if errorlevel 1 exit 1
+
+move bin\minell.exe %LIBRARY_BIN% || exit 1
+move bin\nnbathy.exe %LIBRARY_BIN% || exit 1
+
+exit 0
